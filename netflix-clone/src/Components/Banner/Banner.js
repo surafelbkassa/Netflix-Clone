553b4ded6bb04870f5830e56c69a4e3b3faf5346
Surafel Belachew <148704749+surafelbkassa@users.noreply.github.com>
2025-02-14 09:48:00 +0300

    banner

---
+++ netflix-clone/src/Components/Banner/Banner.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import axios from '../../utils/axios';
+import requests from '../../utils/requests';
+import './banner.css';
+const Banner = () => {
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const request = await axios.get(requests.fetchNetflixOriginals);
+                setMovie(request.data.results[Math.floor(Math.random() * request.data.results.length)]);
+            } catch (error) {
+                console.log("error", error);
+            }
+        })(); // Removed extra parenthesis here
+    }, []); // Added dependency array
+        function truncate(str, n) {
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+    }
+    return (
+        <div className='banner'
+            style={{
+                backgroundSize: "cover",
+                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+                backgroundPosition: "center center",
+                backgroundRepeat: "no-repeat"
+            }}
+        >
+            <div className='banner_content'>
+                <h1 className='banner_title'>{movie?.title || movie?.name || movie?.original_name}</h1>
+                <div className='banner_buttons'>
+                    <button className='banner_button'>Play</button>
+                    <button className='banner_button'>My List</button>
+                </div>
+                <h1 className='banner_description'>{truncate(movie?.overview,150)}</h1>
+            </div>
+            <div className='banner_fadeBottom'></div>
+        </div>
+    );
+}
+
+export default Banner;
+
--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -1,16 +1,14 @@
-
 import React from 'react'
 import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
-
+import Banner from '../../Components/Banner/Banner'
 const Home = () => {
   return (
         <>
             <Header />
-            <h1>Netflix</h1>
+            <Banner/>
             <Footer />
         </>
   )
 }
-
 export default Home
