95f1ce8ecac08db873478f4d5a939dc13df3b33f
Surafel Belachew <148704749+surafelbkassa@users.noreply.github.com>
2025-02-14 06:52:00 +0300

    axios

---
+++ netflix-clone/src/utils/requests.js
@@ -0,0 +1,13 @@
+const API_KEY="0ae966d0e4a33f50750f77a4822d8a4c";
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchTvShows:`tv/popular?api_key=${API_KEY}&language=en-US&page=1`
+};
+export default requests;
