6e9f193c08b7ecc9a8aaa4697336c74da320b7f4
Surafel Belachew <148704749+surafelbkassa@users.noreply.github.com>
2025-02-17 08:28:40 +0300

    starting on the rows

---
+++ netflix-clone/src/Components/Rows/RowList/RowList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Row from '../Row/Row'
+const RowList = () => {
+  return (
+    <div>
+      <Row />
+      <Row />
+      <Row />
+      <Row />
+    </div>
+  )
+}
+
+export default RowList
